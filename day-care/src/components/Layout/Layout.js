69ca57b7fec0cccbd9f1283b0759012ffee497d8
mihaela1354 <mihaela1354@gmail.com>
2020-05-27 00:28:44 +0300

    created layout component

---
+++ day-care/src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Aux from '../../hoc/Aux'
+const layout = (props) => (
+    <Aux>
+        <div> Toolbar,SideDrawer, Backrop</div>
+        <main>
+            {props.chilren}
+        </main>
+    </Aux>
+);
+
+export default layout;
